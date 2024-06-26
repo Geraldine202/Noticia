ac485f9ccc1334a95f46b37158ae79276c0651f3
Geraldine202 <geraldineinostroza1995@gmail.com>
2024-06-25 20:40:28 -0400

    first commit

--- Noticia/settings.py
+++ Noticia/settings.py
@@ -210,4 +210,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'caosnews64@gmail.com'
-EMAIL_HOST_PASSWORD = 'Super1234'
+EMAIL_HOST_PASSWORD = 'lxsb pcjd txys jcoy'
