1149d165d60586345ce08f773d64aa65c976baf5
Geraldine202 <geraldineinostroza1995@gmail.com>
2024-06-25 20:07:48 -0400

    first commit

--- Noticia/settings.py
+++ Noticia/settings.py
@@ -209,5 +209,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'caosnews3@gmail.com'
-EMAIL_HOST_PASSWORD = 'Super1234.'
+EMAIL_HOST_USER = 'caosnews64@gmail.com'
+EMAIL_HOST_PASSWORD = 'Super1234'
